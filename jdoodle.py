2de91b215b328ca14711444a9b087759b600e3af
rmt00 <ricatambor11@gmail.com>
2025-03-04 00:58:02 +0800

    Add files via upload

---
+++ jdoodle.py
@@ -0,0 +1,14 @@
+class humans: 
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+    def show(self):
+        print('name:',self. name)
+        print('age:',self.age)
+        
+Arian = humans('Arian',22)
+Shenna = humans('Shenna',20)
+
+Arian.show()
+Shenna.show()
