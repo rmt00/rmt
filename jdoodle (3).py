18386a2683ac6aad87a5adb5eba44febec40217b
rmt00 <ricatambor11@gmail.com>
2025-03-05 21:48:31 +0800

    Add files via upload

---
+++ jdoodle (3).py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self._name = name 
+        self._age = age   
+
+    def get_name(self):
+        return self._name
+
+    def get_age(self):
+        return self._age
+
+my_person = Person("Ari", 22)
+print(my_person.get_name())
+print(my_person.get_age())  
